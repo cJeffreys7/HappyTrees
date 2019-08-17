c124388f1338c4a677fa800fa4656899d1e9d2f1
Chris Jeffreys <chrisbjeffreys@gmail.com>
2019-08-17 11:56:44 -0500

    Formated paintings in grid format
    Added season and episode tag
    Added name tag

--- HappyTrees/Controllers/PaintingController.cs
+++ HappyTrees/Controllers/PaintingController.cs
@@ -19,7 +19,17 @@ namespace HappyTrees.Controllers
             Painting painting1 = new Painting { Title = "A Walk in the Woods", Season = 1, Episode = 1, Description = "The very first episode of 'The Joy of Painting' with Bob Ross.", ThumbnailFile = "~/css/Thumbnails/S01E01.png" };
             Painting painting2 = new Painting { Title = "Mt. McKinley", Season = 1, Episode = 2, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E02.png" };
             Painting painting3 = new Painting { Title = "Ebony Sunset", Season = 1, Episode = 3, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E03.png" };
-            List<Painting> testPaintings = new List<Painting> { painting1, painting2, painting3 };
+            Painting painting4 = new Painting { Title = "Winter Mist", Season = 1, Episode = 4, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E04.png" };
+            Painting painting5 = new Painting { Title = "Quiet Stream", Season = 1, Episode = 5, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E05.png" };
+            Painting painting6 = new Painting { Title = "Winter Moon", Season = 1, Episode = 6, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E06.png" };
+            Painting painting7 = new Painting { Title = "Autumn Mountain", Season = 1, Episode = 7, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E07.png" };
+            Painting painting8 = new Painting { Title = "Peaceful Valley", Season = 1, Episode = 8, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E08.png" };
+            Painting painting9 = new Painting { Title = "Seascape", Season = 1, Episode = 9, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E09.png" };
+            Painting painting10 = new Painting { Title = "Mountain Lake", Season = 1, Episode = 10, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E10.png" };
+            Painting painting11 = new Painting { Title = "Winter Glow", Season = 1, Episode = 11, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E11.png" };
+            Painting painting12 = new Painting { Title = "Snow Fall", Season = 1, Episode = 12, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E12.png" };
+            Painting painting13 = new Painting { Title = "Final Reflections", Season = 1, Episode = 13, Description = "", ThumbnailFile = "~/css/Thumbnails/S01E13.png" };
+            List<Painting> testPaintings = new List<Painting> { painting1, painting2, painting3, painting4, painting5, painting6, painting7, painting8, painting9, painting10, painting11, painting12, painting13 };
             return View(testPaintings);
         }
 
