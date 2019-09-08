278365546972410520cd8e5224fb4f2090a3e5cb
Chris Jeffreys <chrisbjeffreys@gmail.com>
2019-09-08 16:01:06 -0500

    Implemented PaintingRepository

--- HappyTrees/Controllers/PaintingController.cs
+++ HappyTrees/Controllers/PaintingController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HappyTrees.Models;
+using HappyTrees.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,198 +12,18 @@ namespace HappyTrees.Controllers
     [Route("[controller]/[action]")]
     public class PaintingController : Controller
     {
+        private readonly IPaintingService paintingService;
 
-        #region Paintings List
-        public static List<Painting> paintings = new List<Painting>
+        public PaintingController(IPaintingService paintingService)
         {
-            new Painting
-            {
-                Title = "A Walk in the Woods", Season = 1, Episode = 1,
-                Description = "The very first episode of 'The Joy of Painting' with Bob Ross.",
-                ThumbnailFile = "~/Images/Thumbnails/S01E01.png",
-                VideoURL = "https://www.youtube.com/embed/oh5p5f5_-7A",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite,
-                        Color.BlackGesso                        
-                    }
-            },
-            new Painting
-            {
-                Title = "Mt. McKinley",
-                Season = 1,
-                Episode = 2,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E02.png",
-                VideoURL = "https://www.youtube.com/embed/RInDWhYceLU",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Ebony Sunset",
-                Season = 1,
-                Episode = 3,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E03.png",
-                VideoURL = "https://www.youtube.com/embed/UOziR7PoVco",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite,
-                        Color.BlackGesso
-                    }
-            },
-            new Painting
-            {
-                Title = "Winter Mist",
-                Season = 1,
-                Episode = 4,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E04.png",
-                VideoURL = "https://www.youtube.com/embed/0pwoixRikn4",
-                Colors = new List<Color>
-                    {
-                        Color.VanDykeBrown, Color.PrussianBlue, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Quiet Stream",
-                Season = 1,
-                Episode = 5,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E05.png",
-                VideoURL = "https://www.youtube.com/embed/DFSIQNjKRfk",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Winter Moon",
-                Season = 1,
-                Episode = 6,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E06.png",
-                VideoURL = "https://www.youtube.com/embed/loAzRUzx1wI",
-                Colors = new List<Color>
-                    {
-                        Color.VanDykeBrown, Color.PrussianBlue, Color.TitaniumWhite, Color.BlackGesso
-                    }
-            },
-            new Painting
-            {
-                Title = "Autumn Mountain",
-                Season = 1,
-                Episode = 7,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E07.png",
-                VideoURL = "https://www.youtube.com/embed/sDdpc8uisD0",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Peaceful Valley",
-                Season = 1,
-                Episode = 8,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E08.png",
-                VideoURL = "https://www.youtube.com/embed/kQlFwTOkYzg",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Seascape",
-                Season = 1,
-                Episode = 9,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E09.png",
-                VideoURL = "https://www.youtube.com/embed/QxcS7p1VHyQ",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Mountain Lake",
-                Season = 1,
-                Episode = 10,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E10.png",
-                VideoURL = "https://www.youtube.com/embed/wDnLlywAL5I",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Winter Glow",
-                Season = 1,
-                Episode = 11,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E11.png",
-                VideoURL = "https://www.youtube.com/embed/Q03YvknOVe0",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Snow Fall",
-                Season = 1,
-                Episode = 12,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E12.png",
-                VideoURL = "https://www.youtube.com/embed/4E35-8x_y04",
-                Colors = new List<Color>
-                    {
-                        Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue, Color.TitaniumWhite
-                    }
-            },
-            new Painting
-            {
-                Title = "Final Reflections",
-                Season = 1,
-                Episode = 13,
-                Description = "",
-                ThumbnailFile = "~/Images/Thumbnails/S01E13.png",
-                VideoURL = "https://www.youtube.com/embed/IEQWfszfRlA",
-                Colors = new List<Color>
-                    {
-                        Color.SapGreen, Color.AlizarinCrimson, Color.VanDykeBrown, Color.PrussianBlue,
-                        Color.PhthaloGreen, Color.CadmiumYellow, Color.BrightRed, Color.TitaniumWhite
-                    }
-            }
-        };
-
-        #endregion Painting List
+            this.paintingService = paintingService;
+        }
 
 
         // Show categorized paintings in grid format
         public ActionResult AllPaintings()
         {
-
+            List<Painting> paintings = paintingService.GetAllPaintings();
             return View(paintings);
         }
 
@@ -212,7 +33,7 @@ namespace HappyTrees.Controllers
         public ActionResult PaintingDetails(string title)
         {
             title = title.Replace("-", " ");
-            Painting painting = paintings.FirstOrDefault(p => title == p.Title);
+            Painting painting = paintingService.GetPainting(title);
 
             return View(painting);
         }
---
+++ HappyTrees/Data/IPaintingRepository.cs
@@ -0,0 +1,15 @@
+﻿using HappyTrees.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HappyTrees.Data
+{
+    public interface IPaintingRepository
+    {
+        List<Painting> GetAllPaintings();
+
+        Painting GetPainting(string title);
+    }
+}
